d5d86236216f7d3b14114c3710c67baa014b6bf5
Lautaro Trabajo <lautaro.fuente@yahoo.com>
2024-11-20 14:03:44 -0300

    starting

---
+++ Backend/alimentation-app/src/main/java/com/alimentation_app/alimentation_app/AlimentationAppApplication.java
@@ -0,0 +1,13 @@
+package com.alimentation_app.alimentation_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlimentationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlimentationAppApplication.class, args);
+	}
+
+}
---
+++ Backend/alimentation-app/src/test/java/com/alimentation_app/alimentation_app/AlimentationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alimentation_app.alimentation_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlimentationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
